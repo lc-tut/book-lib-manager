737d3ab36f59102c2a0b77182f4397a569318f6e
kumapana <kumachan05-11@hotmail.co.jp>
2017-12-21 12:00:05 +0900

    fix 12/21 12:00

--- loan_post.php
+++ loan_post.php
@@ -15,26 +15,78 @@
     die();
   }
   echo h($ID)."\n".h($ISBN)."\n".h($dc);
- 
-#  try {
-#    $dbh = getDb();
-#    $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-#    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-#    $sql = "UPDATE books SET title = ?, author = ?, publisher = ?, genre = ? WHERE ISBN = ?";
-#    $stmt = $dbh->prepare($sql);
-#    $stmt->bindValue(1, $title, PDO::PARAM_STR);
-#    $stmt->bindValue(2, $author, PDO::PARAM_STR);
-#    $stmt->bindValue(3, $publisher, PDO::PARAM_STR);
-#    $stmt->bindValue(4, $genre, PDO::PARAM_STR);
-#    $stmt->bindValue(5, $ISBN, PDO::PARAM_STR);
-#    $stmt->execute();
-#    $dbh = null;
-#    echo "蔵書情報の更新が完了しました。<br>";
-#    echo "<a href=\"./\">リストに戻る</a>";
-#  }
-#  catch (Exception $e) {
-#    echo "エラーが発生しました: " . h($e->getMessage()) . "";
-#    echo "<br><a href=\"./\">リストに戻る</a>";
-#    die();
-#  }
+
+  if($dc == 'loan'){
+    try {
+      $dbh = getDb();
+      $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $sql = "UPDATE books SET `status` = `?`, `borrower` = `?` WHERE ISBN = ?";
+      $stmt = $dbh->prepare($sql);
+      $stmt->bindValue(1, '貸出中', PDO::PARAM_STR);
+      $stmt->bindValue(2, $ISBN, PDO::PARAM_STR);
+      $stmt->execute();
+      $sql = 'insert into `'.$ISBN.'` `borrower` = `?`, `dc` = `?`';
+      $stmt = $dbh->prepare($sql);
+      $stmt->bindValue(1, $ID, PDO::PARAM_STR);
+      $stmt->bindValue(2, "貸出", PDO::PARAM_STR);
+      $stmt->execute();
+      $dbh = null;
+
+      #$dbu = getUser();
+      #$dbu->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+      #$dbu->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      #$sql = "UPDATE books SET `status` = `?`, `borrower` = `?` WHERE ISBN = ?";
+      #$stmt = $dbu->prepare($sql);
+      #$stmt->bindValue(1, '貸出中', PDO::PARAM_STR);
+      #$stmt->bindValue(2, '部室内書庫', PDO::PARAM_STR);
+      #$stmt->execute();
+      #$dbu = null;
+
+      echo "貸出処理が完了しました。<br>";
+      echo "<a href=\"./\">リストに戻る</a>";
+    }
+    catch (Exception $e) {
+      echo "エラーが発生しました: " . h($e->getMessage()) . "";
+      echo "<br><a href=\"./\">リストに戻る</a>";
+      die();
+    }
+  }
+  else if($dc == 'return'){
+    try {
+      $dbh = getDb();
+      $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $sql = "UPDATE books SET `status` = `?`, `borrower` = `?` WHERE ISBN = ?";
+      $stmt = $dbh->prepare($sql);
+      $stmt->bindValue(1, '部室内書庫', PDO::PARAM_STR);
+      $stmt->bindValue(2, $ISBN, PDO::PARAM_STR);
+      $stmt->execute();
+      $sql = 'insert into `'.$ISBN.'` `borrower` = `?`, `dc` = `?`';
+      $stmt = $dbh->prepare($sql);
+      $stmt->bindValue(1, $ID, PDO::PARAM_STR);
+      $stmt->bindValue(2, "返却", PDO::PARAM_STR);
+      $stmt->execute();
+      $dbh = null;
+
+      #$dbu = getUser();
+      #$dbu->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+      #$dbu->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      #$sql = "UPDATE books SET `status` = `?`, `borrower` = `?` WHERE ISBN = ?";
+      #$stmt = $dbu->prepare($sql);
+      #$stmt->bindValue(1, '貸出中', PDO::PARAM_STR);
+      #$stmt->bindValue(2, '部室内書庫', PDO::PARAM_STR);
+      #$stmt->execute();
+      #$dbu = null;
+
+      echo "貸出処理が完了しました。<br>";
+      echo "<a href=\"./\">リストに戻る</a>";
+    }
+    catch (Exception $e) {
+      echo "エラーが発生しました: " . h($e->getMessage()) . "";
+      echo "<br><a href=\"./\">リストに戻る</a>";
+      die();
+    }
+
+  }
 ?>
