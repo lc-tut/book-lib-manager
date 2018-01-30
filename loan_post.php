255dcb31241c642ee504a805450cf0c9fe1c7a60
kumapana <kumachan05-11@hotmail.co.jp>
2018-01-30 17:15:28 +0900

    fix 01/30 17:15

--- loan_post.php
+++ loan_post.php
@@ -54,7 +54,7 @@
       $sql = "UPDATE books SET `status` = ?, `borrower` = ? WHERE ISBN = ?";
       $stmt = $dbh->prepare($sql);
       $stmt->bindValue(1, '部室内書庫', PDO::PARAM_STR);
-      $stmt->bindValue(2, $ID, PDO::PARAM_STR);
+      $stmt->bindValue(2, '貸出可能', PDO::PARAM_STR);
       $stmt->bindValue(3, $ISBN, PDO::PARAM_STR);
       $stmt->execute();
       $sql = "insert into `history` (ID, date_time, ISBN, processer, process) values (?, ?, ?, ?, ?)";
