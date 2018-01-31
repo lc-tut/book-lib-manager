9081a78978d4ec70d162d5b2e41927715fde2736
kumapana <kumachan05-11@hotmail.co.jp>
2018-01-31 17:39:38 +0900

    fix 01/31 17:39

--- test.php
+++ test.php
@@ -16,4 +16,34 @@
       }
       $row = $stmh->fetch(PDO::FETCH_ASSOC);
       echo $row['status'];
+      $db = null;
+
+      try{
+        require_once 'DbManager.php';
+        $db = getDb();
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+      }catch(PDOException $Exception){
+        die('接続エラー：' .$Exception->getMessage());
+      }
+      try{
+        $sql = "select * from books";
+        $stmh = $db->prepare($sql);
+        $stmh->execute();
+      }catch(PDOException $Exception){
+        die('接続エラー：' .$Exception->getMessage());
+      }
+      while($row = $stmh->fetch(PDO::FETCH_ASSOC)){
+        $userData[]=array(
+        'ISBN'=>$row['ISBN'],
+        'title'=>$row['title'],
+        'author'=>$row['author'],
+        'publisher'=>$row['publisher'],
+        'genre'=>$row['genre'],
+        'status'=>$row['status'],
+        'borrower'=>$row['borrower']
+        );
+    }
+    header('Content-type: application/json');
+    echo json_encode($books_data);
 ?>
