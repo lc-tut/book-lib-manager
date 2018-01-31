e5f4971b66e9ebd478f32a1535afb68fd808295c
kumapana <kumachan05-11@hotmail.co.jp>
2018-01-31 17:21:52 +0900

    fix 01/31 17:21

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+      try{
+        require_once 'DbManager.php';
+        $db = getDb();
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+      }catch(PDOException $Exception){
+        die('接続エラー：' .$Exception->getMessage());
+      }
+      try{
+        $sql = "select status from books where ISBN like '9784791960279';";
+        $stmh = $db->prepare($sql);
+        $stmh->execute();
+      }catch(PDOException $Exception){
+        die('接続エラー：' .$Exception->getMessage());
+      }
+      echo var_dump($stmh);
+?>
