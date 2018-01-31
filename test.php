38824a628acac5b6581f72541f6ac574dde4e161
kumapana <kumachan05-11@hotmail.co.jp>
2018-01-31 17:45:45 +0900

    fix 01/31 17:45

--- test.php
+++ test.php
@@ -1,24 +1,22 @@
 <?php
-      /*
-      try{
-        require_once 'DbManager.php';
-        $db = getDb();
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-      }catch(PDOException $Exception){
-        die('接続エラー：' .$Exception->getMessage());
-      }
-      try{
-        $sql = "select status from books where ISBN like '9784791960279';";
-        $stmh = $db->prepare($sql);
-        $stmh->execute();
-      }catch(PDOException $Exception){
-        die('接続エラー：' .$Exception->getMessage());
-      }
-      $row = $stmh->fetch(PDO::FETCH_ASSOC);
-      echo $row['status'];
-      $db = null;
-      */
+     # try{
+     #   require_once 'DbManager.php';
+     #   $db = getDb();
+     #   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     #   $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+     # }catch(PDOException $Exception){
+     #   die('接続エラー：' .$Exception->getMessage());
+     # }
+     # try{
+     #   $sql = "select status from books where ISBN like '9784791960279';";
+     #   $stmh = $db->prepare($sql);
+     #   $stmh->execute();
+     # }catch(PDOException $Exception){
+     #   die('接続エラー：' .$Exception->getMessage());
+     # }
+     # $row = $stmh->fetch(PDO::FETCH_ASSOC);
+     # echo $row['status'];
+     # $db = null;
 
       try{
         require_once 'DbManager.php';
