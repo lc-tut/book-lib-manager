ffe42c5cb7436910ce7f2338c342c207caf48d45
kumapana <kumachan05-11@hotmail.co.jp>
2018-02-02 13:52:52 +0900

    fix 02/02 13:52

--- detail.php
+++ detail.php
@@ -14,11 +14,21 @@ $openbd_data = json_decode(file_get_contents("https://api.openbd.jp/v1/get?isbn=
 
 <!DOCTYPE html>
 <html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <title>書籍詳細</title>
-    </head>
-    <body>
-        <h1>書籍詳細</h1>
-        <img src="<?php he($openbd_data[0]->onix->CollateralDetail->SupportingResource[0]->ResourceVersion[0]->ResourceLink)?>" alt="書写">
-    </body>
+<head>
+    <meta charset="utf-8">
+    <title>書籍詳細</title>
+</head>
+<body>
+    <h1>書籍詳細</h1>
+    <img src="<?php he($openbd_data[0]->onix->CollateralDetail->SupportingResource[0]->ResourceVersion[0]->ResourceLink)?>" alt="書写">
+    <table>
+        <tr>
+            <td>タイトル</td>
+            <td><?he($openbd_data[0]->onix->DescriptiveDetail->TitleDetail->TitleElement->TitleText->content)?></td>
+        </tr>
+        <tr>
+            <td>タイトル (ヨミ)</td>
+            <td><?he($openbd_data[0]->onix->DescriptiveDetail->TitleDetail->TitleElement->TitleText->collationkey)?></td>
+        </tr>
+    </table>
+</body>
